5d0b0c080f909f427841a8e034de527e8eb6b389
Irene <25118334+isteves@users.noreply.github.com>
2019-01-11 07:37:36 -0500

    sample puzzle

---
+++ 11_sandwiches/11_soln.R
@@ -0,0 +1,16 @@
+#' ---
+#' title: Sandwiches
+#' ---
+#'
+#' Use _Ctrl (Cmd) + Shift + K_ to render this file
+#' 
+#+ r setup, include = FALSE
+options(tidyverse.quiet = TRUE)
+
+#+ r
+library(tidyverse)
+library(here)
+data_path <- here::here('11_sandwiches', '11_data.csv')
+
+# YOUR SOLUTION CODE HERE
+
